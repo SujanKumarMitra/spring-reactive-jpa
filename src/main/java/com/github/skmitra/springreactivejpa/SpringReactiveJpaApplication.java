7b63c35b063a318f8936b503668d171027592c63
skmitra <mitrakumarsujan@gmail.com>
2021-08-15 22:43:32 +0530

    Init project

---
+++ src/main/java/com/github/skmitra/springreactivejpa/SpringReactiveJpaApplication.java
@@ -0,0 +1,13 @@
+package com.github.skmitra.springreactivejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveJpaApplication.class, args);
+	}
+
+}
