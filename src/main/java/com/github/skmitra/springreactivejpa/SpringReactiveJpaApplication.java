856b4db69afc7ce57f3986a6eba6d07d7b8688b7
skmitra <mitrakumarsujan@gmail.com>
2021-08-16 09:01:04 +0530

    added blockhound dependency

--- src/main/java/com/github/skmitra/springreactivejpa/SpringReactiveJpaApplication.java
+++ src/main/java/com/github/skmitra/springreactivejpa/SpringReactiveJpaApplication.java
@@ -2,11 +2,13 @@ package com.github.skmitra.springreactivejpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class SpringReactiveJpaApplication {
 
 	public static void main(String[] args) {
+		BlockHound.install();
 		SpringApplication.run(SpringReactiveJpaApplication.class, args);
 	}
 
